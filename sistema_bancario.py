bd1a739591aef8719283bd656484b47dc760b9a0
Ana Laura <analauracnunes@icloud.com>
2025-03-01 13:41:06 -0300

    Update sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -3,6 +3,8 @@ saldo = 0
 deposito = 0 
 saque = 0 
 extrato = ""
+valor_total_circulante=0
+limite = 500
 quantidade_de_saques_disponiveis = 3
 while True:
     print("""Para acessar as operações clique em :
@@ -10,7 +12,8 @@ while True:
           2 para sacar
           3 para conferir o saldo atual
           4 para verificar o extrato
-          5 para sair
+          5 para revisar limites
+          6 para sair
           """)
     time.sleep(2)
     operação = int(input("Digite aqui a operação desejada"))
@@ -21,11 +24,12 @@ while True:
                     depósito no valor de R${deposito}
                     totalizando um valor de {saldo}"""
         print("Depósito realizado com sucesso")
+        valor_total_circulante += saldo
         time.sleep(1)
     elif operação ==2:
         if quantidade_de_saques_disponiveis > 0 :
             saque = float(input("Digite o valor que você deseja sacar"))
-            if saque <= 500:
+            if saque <= limite:
                 saldo -= saque
                 quantidade_de_saques_disponiveis -= 1
                 extrato += f"""
@@ -47,6 +51,44 @@ while True:
               {extrato}""")
         time.sleep(6)
     elif operação == 5:
+        print(f"""Atualmente você pode fazer {quantidade_de_saques_disponiveis} saques de até {limite} reais
+Caso você queira aumentar a quantidade de saques digite 1 
+Caso queira aumetar o limite de saques digite 2 
+Caso queira sair digite outro número""")
+        time.sleep(2)
+        operação_limite = int(input("Digite a operação desejada"))
+        if operação_limite == 1:
+            print("Iniciando a avaliação de limites")
+            time.sleep(2)
+            print(f" A nova quantidade proposta é de {(quantidade_de_saques_disponiveis + valor_total_circulante//4000):.0}")
+            time.sleep(1)
+            print("""Caso queira aceitar o novo limite digite 1, 
+caso não, digite outro número""")
+            if int(input("Quer aceitar ou não")) == 1:
+                print(" Seu novo limite foi estabelecido")
+                quantidade_de_saques_disponiveis += valor_total_circulante//4000
+                time.sleep(2)
+            else:
+                print(" O seu limite se manteve o mesmo")
+                time.sleep(2)
+        elif operação_limite == 2:
+            print("Iniciando a avaliação de limites")
+            time.sleep(2)
+            print(f" O novo limite proposto é de {(limite + valor_total_circulante // 400)} reais")
+            time.sleep(1)
+            print("""Caso queira aceitar o novo limite digite 1,
+caso não, digite outro número""")
+            if int(input("Quer aceitar ou não")) == 1:
+                print(" Seu novo limite foi estabelecido")
+                limite += valor_total_circulante // 400
+                time.sleep(2)
+            else:
+                print(" O seu limite se manteve o mesmo")
+                time.sleep(2)
+        else:
+            print("Você saiu da área de limites")
+            time.sleep(2)
+    elif operação == 6:
         print("Obrigada por usar nossa plataforma !")
         break
     else:
